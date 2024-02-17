fd947fc4db938614b8691d0f7a3bcfc71c0ce348
mykytaolin <olin.mykyta@gmail.com>
2024-02-17 18:41:04 +0100

    I did base structure of game field and blocks

--- tetris.c
+++ tetris.c
@@ -1,3 +1,30 @@
-//
-// Created by olinm on 17.02.2024.
-//
+#include "tetris.h"
+#include <stdlib.h>
+
+#ifndef CTETRIS_TETRIS_H
+#define CTETRIS_TETRIS_H
+
+TetField* createTetField(int width, int height) {
+    TetField* tetf = (TetField*)malloc(sizeof(TetField));
+    tetf->width = width;
+    tetf->height = height;
+    tetf->blocks = (TetBlock*)malloc(sizeof(TetBlock)*width*height);
+    for(int i=0, i<width*height, i++)
+        tetf->blocks[i].b = 0;
+
+    return testf;
+};
+
+TetGame* createTetGame(int field_width, int field_height, int figure_size,
+                       int count, TetBlock* figures_template) {
+
+    TetGame* tetg = (TetGame*)malloc(sizeof(TetGame));
+    tetg->field = createTetField(field_width, field_height);
+    tetg->figurest = createTetFiguresT( count, figures_size, figures_template);
+
+    return tetg;
+};
+
+void calculateTet(TetGame* tetg);
+
+#endif
--- tetris.h
+++ tetris.h
@@ -1,8 +1,42 @@
-//
-// Created by olinm on 17.02.2024.
-//
+#ifndef CTETRIS_TETRIS_H
+#define CTETRIS_TETRIS_H
 
-#ifndef UNTITLED1_TETRIS_H
-#define UNTITLED1_TETRIS_H
+typedef struct TetBlock {
+    int b;
+}TetBlock;
 
-#endif //UNTITLED1_TETRIS_H
+typedef struct TetFigure {
+    int x;
+    int y;
+    int size;  // field behind a falling figure is a square with size of this variable
+    TetBlock* blocks;  // this variable will contain the list of blocks relevant to this figure
+}TetFigure;
+
+typedef struct TetFiguresT {  // for describing the figure we have to have:
+    int count;  // number of this figures
+    int size;  // length of the side of square field on which this figure is placed
+    TetBlock* blocks;  // and list of blocks,
+    // block variable is variable which refers to unbreakable memory field which contain an info about,
+    // all block templates
+}TetFiguresT;
+
+typedef struct TetField {  // containing height and width of game field and variable which contain list of blocks
+    int width;
+    int height;
+    TetBlock* blocks;
+
+}TetField;
+
+typedef struct TetGame {
+    TetField* field;  // information about game field and info about block on it
+    TetFigure* figure; // info about type of falling figure
+    TetFiguresT* figurest;  // info about all figures on the game field
+}TetGame;
+
+TetGame* createTetGame(int field_width, int field_height, int figure_size,
+                       int count, TetBlock* figures_template);  // protorype of function which init base game structure
+
+void calculateTet(TetGame* tetg);
+
+
+#endif //CTETRIS_TETRIS_H
